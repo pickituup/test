83ab7d901aa0e7ebe1f114be60e5955e001e2018
Vladimir Ivanov <ivanov_va@hotmail.com>
2017-09-11 12:15:12 +0300

    Add SQliteService and Renderers

---
+++ tpm/tpm/tpm.iOS/Renderers/ExtendedContentViewRenderer.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using tpm.iOS.Renderers;
+using tpm.Views.Controls;
+using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ExportRenderer(typeof(ExtendedContentView), typeof(ExtendedContentViewRenderer))]
+namespace tpm.iOS.Renderers {
+    public class ExtendedContentViewRenderer : VisualElementRenderer<ExtendedContentView> {
+
+        private ExtendedContentView _element;
+
+        protected override void OnElementChanged(ElementChangedEventArgs<ExtendedContentView> e) {
+            base.OnElementChanged(e);
+
+            if (e.NewElement != null) {
+
+                _element = e.NewElement as ExtendedContentView;
+                SetupLayer(_element.BorderThickness, _element.CornerRadius);
+            }
+        }
+
+        private void SetupLayer(int borderWidth, nfloat borderRadius) {
+
+            Layer.CornerRadius = borderRadius;
+
+            if (Element.BackgroundColor != Color.Default) {
+                Layer.BackgroundColor = Element.BackgroundColor.ToCGColor();
+            } else {
+                Layer.BackgroundColor = UIColor.White.CGColor;
+            }
+
+            if (Element.BorderColor != Color.Default) {
+                Layer.BorderColor = Element.BorderColor.ToCGColor();
+                Layer.BorderWidth = borderWidth;
+            } else {
+                Layer.BackgroundColor = UIColor.Clear.CGColor;
+            }
+
+            Layer.RasterizationScale = UIScreen.MainScreen.Scale;
+            Layer.ShouldRasterize = true;
+        }
+    }
+}
