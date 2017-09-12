522ea6091f5e7f11b573c7d6d6cf86d947510a4f
Artem Tuchonuk <irascible.dog@gmail.com>
2017-09-12 20:35:08 +0300

    Rembo 5

--- tpm/tpm/tpm/DependencyServices/IFileHelper.cs
+++ tpm/tpm/tpm/DependencyServices/IFileHelper.cs
@@ -7,40 +7,6 @@ using System.Threading.Tasks;
 
 namespace tpm.DependencyServices {
     public interface IFileHelper {
-        /*
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <param name="fileName"></param>
-        /// <returns></returns>
-        bool Exists(string fileName);
-
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <param name="fileName"></param>
-        /// <param name="text"></param>
-        void WriteText(string fileName, string text);
-
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <param name="fileName"></param>
-        /// <returns></returns>
-        string ReadText(string fileName);
-
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <returns></returns>
-        IEnumerable<string> GetFiles();
-
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <param name="fileName"></param>
-        void Delete(string fileName);
-        */
 
         /// <summary>
         ///
