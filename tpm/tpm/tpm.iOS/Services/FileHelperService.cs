297e24a8c024c0a12d3add234bb3117377337699
Artem Tuchonuk <irascible.dog@gmail.com>
2017-09-13 11:32:15 +0300

    test

--- tpm/tpm/tpm.iOS/Services/FileHelperService.cs
+++ tpm/tpm/tpm.iOS/Services/FileHelperService.cs
@@ -61,24 +61,8 @@ namespace tpm.iOS.Services {
         /// <returns></returns>
         public Task<bool> DownloadSourceAsync(string src) {
             return Task<bool>.Run(() => {
-                try {
-                    HttpWebRequest request = new HttpWebRequest(new Uri(src));
-
-                    using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
-                    using (Stream stream = response.GetResponseStream())
-                    using (FileStream fileStream = System.IO.File.Open(System.IO.Path.Combine(GetInternalTpmDirPath(), DOWNLOADED_PDF_FILE_NAME), FileMode.Create)) {
-                        //
-                        // Clear all data from that file.
-                        //
-                        fileStream.SetLength(0);
-                        stream.CopyTo(fileStream);
-                    }
-
-                    return true;
-                }
-                catch (Exception e) {
-                    return false;
-                }
+                //return TEST_1(src);
+                return TEST_2(src);
             });
         }
 
@@ -106,5 +90,51 @@ namespace tpm.iOS.Services {
         /// <returns></returns>
         public bool IsFileExists(string fullFilePath) =>
             File.Exists(fullFilePath);
+
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="src"></param>
+        /// <returns></returns>
+        private bool TEST_1(string src) {
+            try {
+                HttpWebRequest request = new HttpWebRequest(new Uri(src));
+
+                using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
+                using (Stream stream = response.GetResponseStream())
+                using (FileStream fileStream = System.IO.File.Open(System.IO.Path.Combine(GetInternalTpmDirPath(), DOWNLOADED_PDF_FILE_NAME), FileMode.Create)) {
+                    //
+                    // Clear all data from that file.
+                    //
+                    fileStream.SetLength(0);
+                    stream.CopyTo(fileStream);
+                }
+
+                return true;
+            }
+            catch (Exception e) {
+                return false;
+            }
+        }
+
+        /// </summary>
+        /// <param name="src"></param>
+        /// <returns></returns>
+        private bool TEST_2(string src) {
+            try {
+                string fullFilePath =
+                    System.IO.Path.Combine(GetInternalTpmDirPath(), DOWNLOADED_PDF_FILE_NAME);
+
+                WebClient webClient = new WebClient();
+                webClient.DownloadFile(new Uri(src), fullFilePath);
+
+                return true;
+            }
+            catch (Exception e) {
+                return false;
+            }
+        }
     }
 }
